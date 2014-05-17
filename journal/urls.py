5191eeaaf9221e58629a15d21f677ddb7d96a6e4
Denne Reed <denne.reed@gmail.com>
2014-05-16 21:48:30 -0500

    added journal content view

--- journal/urls.py
+++ journal/urls.py
@@ -3,9 +3,12 @@ from journal import views
 
 urlpatterns = patterns('',
                        # ex /journal/
-                       #url(r'^$', views.JournalView.as_view(), name='journal'),
-                       # ex /meetings/2013/
-                       #url(r'^(?P<year>\d{4})/$', views.MeetingsDetailView.as_view(), name='meeting_detail'),
+                       url(r'^$', views.JournalHome.as_view(), name='journal'),
+                       # ex /journal/current_volume/
+                       #url(r'^current_volume/$', views.JournalCurrent.as_view(), name='current'),
+                       # ex /journal/2013/
+                       url(r'^(?P<year>\d{4})/$', views.JournalVolumes.as_view(), name='volumes'),
+
                        # ex /meetings/abstract/add/
                        #url(r'^abstract/add/$', views.AbstractCreateView1.as_view(), name='create_abstract'),
                        #url(r'^abstract/add/$', 'meetings.views.create_abstract', name='create_abstract'),
--- journal/views.py
+++ journal/views.py
@@ -8,11 +8,41 @@ from fiber.views import FiberPageMixin
 #from django.core.mail import send_mail
 
 
-class JournalView(FiberPageMixin, generic.ListView):
-    # default template name is 'meetings/meeting_list.html'
+class JournalHome(FiberPageMixin, generic.ListView):
     model = Content
+    template_name = 'journal/journal_home.html'
 
     def get_fiber_page_url(self):
         return reverse('journal:journal')
 
 
+class JournalCurrent(FiberPageMixin, generic.ListView):
+    # default template name is 'meetings/meeting_list.html'
+    model = Content
+    template_name = 'journal/journal_home.html'
+
+
+    def get_fiber_page_url(self):
+        return reverse('journal:journal')
+
+
+class JournalVolumes(FiberPageMixin, generic.ListView):
+    # default template name is 'meetings/meeting_list.html'
+    template_name = 'journal/journal.html'
+    context_object_name = 'content_list'
+
+    def get_queryset(self):
+        queryset = Content.objects.filter(year__exact=self.kwargs['year'])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        year = self.kwargs['year']
+        context = super(JournalVolumes, self).get_context_data(**kwargs)
+        context['abstracts'] = Content.objects.filter(year=year, article_type="Annual Meeting Abstracts").order_by('start_page_n')
+        context['articles'] = Content.objects.filter(year=year, article_type="Articles").order_by('start_page_n')
+        context['data'] = Content.objects.filter(year=year, article_type="Data").order_by('start_page_n')
+        context['reviews'] = Content.objects.filter(year=year, article_type="Reviews").order_by('start_page_n')
+        return context
+
+    def get_fiber_page_url(self):
+        return reverse('journal:volumes', kwargs={'year':self.kwargs['year']})
