655d6330b255505afb3cb932acbf4f0b42056fa6
Denne Reed <denne.reed@gmail.com>
2014-07-02 11:32:39 -0500

    Cleaned up comments

--- base/tests.py
+++ base/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from base.models import Member, Announcement, Membership
 from fiber.models import Page
