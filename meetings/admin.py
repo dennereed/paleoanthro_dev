c2a80972a94a0b460a868c75c8f39b6360390f16
Denne Reed <denne.reed@gmail.com>
2014-07-10 17:12:21 -0500

    Add admin action for meeting abstract reporting.

--- meetings/admin.py
+++ meetings/admin.py
@@ -2,7 +2,98 @@ from django.contrib import admin
 from models import *
 from django import forms
 from django.forms.widgets import Textarea
+from django.http import HttpResponse
+from django.template import loader, Context
+import unicodecsv
 
+
+###########################
+# Abstract Report Actions #
+###########################
+
+def create_abstract_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="PaleoAnthro_abstracts.csv"'
+    writer = unicodecsv.writer(response)
+    writer.writerow(['id', 'contact_email', 'presentation_type', 'title', 'abstract_text', 'acknowledgements', 'references', 'comments', 'funding', 'year', 'abstract_rank', 'authors'])
+
+    for abstract in queryset.all():
+        author_list=[]
+        for a in abstract.author_set.all().order_by('author_rank'):
+            author_list.append(a.name)
+        writer.writerow([abstract.id,
+                         abstract.contact_email,
+                         abstract.presentation_type,
+                         abstract.title,
+                         abstract.abstract_text,
+                         abstract.acknowledgements,
+                         abstract.references,
+                         abstract.comments,
+                         abstract.funding,
+                         abstract.year,
+                         abstract.abstract_rank,
+                         ', '.join(author_list)
+        ])
+
+    return response
+create_abstract_csv.short_description = "Download .csv"
+
+
+def create_abstract4meeting_html(modeladmin, request, queryset):
+    abstracts=queryset
+    for abstract in abstracts:
+        abstract.authors=abstract.author_set.all().order_by("author_rank")
+        if len(abstract.authors) > 2:
+            try:
+                abstract.authors=abstract.authors[0].name.split()[-1]+" et al." # get the last name
+            except IndexError:
+                abstract.authors=abstract.authors[0].name.split()[0]+" et al."
+        if len(abstract.authors) == 2:
+            try:
+                abstract.authors=abstract.authors[0].name.split()[-1]+" and "+abstract.authors[1].name.split()[-1]
+            except IndexError:
+                abstract.authors=abstract.authors[0].name.split()[0]+" and "+abstract.authors[1].name.split()[0]
+        if len(abstract.authors) == 1:
+            try:
+                abstract.authors=abstract.authors[0].name.split()[-1] # get the last name
+            except IndexError:
+                abstract.authors=abstract.authors[0].name.split()[0] # get the last name
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="PaleoAnthro_abstracts_for_meeting.html"'
+    t=loader.get_template("meetings/abstract_meeting_template.html")
+    c=Context({
+        'data':abstracts,
+    })
+    response.write(t.render(c))
+    return response
+create_abstract4meeting_html.short_description="Download .html for meeting"
+
+
+def create_abstract4publication_html(modeladmin, request, queryset):
+    abstracts=queryset
+    for abstract in abstracts:
+        abstract.authors = abstract.author_set.all().order_by("author_rank")
+        try:
+            lead_last = abstract.authors[0].name.split()[-1]  # get the lead authors last name
+        except IndexError:
+            lead_last = abstract.authors[0].name.split()[0]  # get the last name if there is only one name
+        abstract.lead_last_name = lead_last
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="PaleoAnthro_abstracts_for_publication.html"'
+    t = loader.get_template("paleoanthro/abstract_publication_template.html")
+    c = Context({
+        'data': abstracts,
+    })
+    response.write(t.render(c))
+    return response
+create_abstract4publication_html.short_description = "Download .html for publication"
+
+
+#################
+# Admin Classes #
+#################
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email_address', 'abstract', 'author_rank',)
@@ -30,7 +121,7 @@ class AbstractAdmin(admin.ModelAdmin):
     search_fields = ['title', 'author__name']
     form = AbstractAdminForm
     inlines = [AuthorInline, ]
-    #actions = [create_abstract_csv, create_abstract4meeting_html, create_abstract4publication_html]
+    actions = [create_abstract_csv, create_abstract4meeting_html, create_abstract4publication_html]
 
 
 class AbstractInline(admin.TabularInline):
