dbfc88d5fe35a21be78f6a1c119f7b62d9888311
Denne Reed <denne.reed@gmail.com>
2014-07-10 15:03:55 -0500

    Updated home page layout. Added random image with custom template tag.

--- base/templatetags/cms_content.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from fiber.models import ContentItem
-
-register = template.Library()
-
-@register.simple_tag()
-def cms_content(name):
-    content = ContentItem.objects.get(name=name)
-    return content.content_html
--- data/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from data import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.IndexView.as_view(), name='index'),
-                       )
-
